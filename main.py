0a95906e373d8880304d96e628a9ee36849c3060
in1tiate <radwoodward@vikings.grayson.edu>
2020-11-09 12:33:06 -0600

    WIP parent mode, switch to choose dir

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def aspect_ratio_str(width: int, height: int) -> str:
 
 root = tk.Tk()
 root.title('JunieTool')
-root.iconbitmap(resource_path('icon.ico'))
+#root.iconbitmap(resource_path('icon.ico'))
 
 frame_select = tk.Frame(root, bd=1, padx=12,
                         pady=3, relief="raised", width=480)
@@ -69,26 +69,51 @@ selected_img_info.grid(row=2, sticky="ew")
 sel_w, sel_h = (1, 1)
 files = []
 file_open = False
+indir = ''
+parent_indir = ''
+seq_dirs = []
+custom_outdir = ''
 
+def select_meta():
+    if parent_mode.get() == 1:
+        select_parent_indir()
+    else:
+        select_frame_sequence()
 
-def select_frame_sequence():  # handle file browse dialog
+def select_parent_indir():
     global sel_w
     global sel_h
+    global file_open
+    global parent_indir
+    global seq_dirs
+    parent_indir = filedialog.askdirectory(parent=root, mustexist=True, title='Choose the directory containing your frame sequences.')
+    if not parent_indir:
+        return None
+    seq_dirs = [parent_indir + os.sep + x for x in os.listdir(parent_indir) if not os.path.isfile(x)]
+    selected_seq_info.config(  # update info labels
+        text="Selected parent " + parent_indir + " (" + str(len(seq_dirs)) + " total directories)")
+    im = Image.open(seq_dirs[0] + os.sep + os.listdir(seq_dirs[0])[0])
+    sel_w, sel_h = im.size
+    selected_img_info.config(text="")
+    file_open = True
+    # check if we already have a desired size before enabling render button
+    if entry_h.get() != "" and entry_w.get() != "":
+        button_ffmpeg.config(state="normal")
+
+def select_frame_sequence():
+    global sel_w
+    global sel_h
+    global indir
     global files
     global file_open
-    file = filedialog.askopenfile(
-        parent=root, mode='rb', title='Choose any image in the sequence', filetypes=[('PNG Images', ['.png'])])
-    if not file:
+    indir = filedialog.askdirectory(parent=root, mustexist=True, title='Choose the directory your sequence is in')
+    if not indir:
         return None
-    dir_ = os.path.dirname(file.name)
-    filetype = os.path.splitext(file.name)  # get extension of selected file
-    files = [os.path.abspath(os.path.join(dir_, f))
-             for f in os.listdir(dir_)
-             if f.endswith(filetype)]  # create list of every file in same dir w/ same extension as selected file
+    files = [x for x in os.listdir(indir) if x.endswith(".png")]
     files = sort_human(files)
     selected_seq_info.config(  # update info labels
-        text="Selected " + os.path.basename(file.name) + " (" + str(len(files)) + " total images)")
-    im = Image.open(files[0])
+        text="Selected " + files[0] + " (" + str(len(files)) + " total images)")
+    im = Image.open(indir + os.sep + files[0])
     sel_w, sel_h = im.size
     selected_img_info.config(
         text=str(sel_w)+"x"+str(sel_h)+", "+aspect_ratio_str(sel_w, sel_h))
@@ -97,9 +122,14 @@ def select_frame_sequence():  # handle file browse dialog
     if entry_h.get() != "" and entry_w.get() != "":
         button_ffmpeg.config(state="normal")
 
+def select_custom_outdir(): # browse for a custom output folder
+    global custom_outdir
+    custom_outdir = filedialog.askdirectory(parent=root, title='Choose an output directory', mustexist=True)
+    if not custom_outdir:
+        custom_outdir = ''
 
 button_browse = tk.Button(
-    frame_select, text='Browse...', command=select_frame_sequence, width=15)
+    frame_select, text='Browse...', command=select_meta, width=15)
 button_browse.grid(row=0, sticky="ew")  # ew, sticky
 
 
@@ -111,14 +141,20 @@ def update_desired_ratio():  # called whenever the w/h entries are edited, keep 
 
 # no, an "intvar" and an int var are not the same thing
 # yes, i am annoyed about it
+parent_mode = tk.IntVar()
+parent_mode.set(0)
 crop_h = tk.IntVar()
 crop_h.set(1)
 overwrite_og = tk.IntVar()
 overwrite_og.set(0)
+use_custom_outdir = tk.IntVar()
+use_custom_outdir.set(0)
 
 frame_options = tk.Frame(root, bd=1, relief="raised",
                          padx=12, pady=3)
 frame_options.grid(row=2, column=0, sticky="nesw")
+check_parent_mode = tk.Checkbutton(frame_options, text="Parent mode", variable=parent_mode, onvalue=True, offvalue=False)
+check_parent_mode.pack(anchor="w")
 check_overwrite_og = tk.Checkbutton(
     frame_options, text='Overwrite source images', variable=overwrite_og, onvalue=True, offvalue=False)
 check_overwrite_og.pack(anchor="w")
@@ -126,9 +162,21 @@ radio_crop_h = tk.Radiobutton(
     frame_options, text="Crop by height", variable=crop_h, value=1).pack(anchor="w")
 radio_crop_w = tk.Radiobutton(
     frame_options, text="Crop by width", variable=crop_h, value=0).pack(anchor="w")
+check_custom_outdir = tk.Checkbutton(
+    frame_options, text='Use custom output folder', variable=use_custom_outdir, onvalue=True, offvalue=False)
+check_custom_outdir.pack(anchor="w")
 
+browse_custom_outdir = tk.Button(frame_options, text="Browse...", command=select_custom_outdir, width=15)
+browse_custom_outdir.pack(anchor="w")
 
-def ffmpeg_export():  # the real meat, this is where i struggle with ffmpeg-python and occasionally succeed
+def ffmpeg_export_meta():
+    if parent_mode.get() == 1:
+        for x in seq_dirs:
+            ffmpeg_export(x)
+    else:
+        ffmpeg_export(indir)
+
+def ffmpeg_export(p_indir):  # the real meat, this is where i struggle with ffmpeg-python and occasionally succeed
     des_w = int(entry_w.get())
     des_h = int(entry_h.get())
     if (des_w > sel_w) or (des_h > sel_h):
@@ -149,18 +197,21 @@ def ffmpeg_export():  # the real meat, this is where i struggle with ffmpeg-pyth
         adj_h = des_ratio * sel_w
         y_offset = (sel_h - adj_h) / 2
     for x in files:
+        x = p_indir + os.sep + x
         progress_ffmpeg.config(text='Rendering: ' + os.path.split(x)[1])
         frame_ffmpeg.update()
         # ffmpeg complains if we try to output to the same file as our input
         # so we output to a different file and replace the input afterwards
         outdir = x + '~.png'
-        if overwrite_og.get() == 0:
+        if overwrite_og.get() == 0 and use_custom_outdir.get() == 0:
             newdir = os.path.dirname(str(x)) + '_' + \
                 str(des_w) + 'x' + str(
                     des_h)  # results in {old directory}_{width}x{height} in the old directory's parent dir
             outdir = newdir + os.sep + str(os.path.split(x)[1])
             if not os.path.isdir(newdir):
                 os.mkdir(newdir)
+        elif use_custom_outdir.get() == 1:
+            outdir = custom_outdir + os.sep + str(os.path.split(x)[1])
         stream = ffmpeg.input(str(x), nostdin=None)
         stream = ffmpeg.crop(stream, x_offset, y_offset, adj_w, adj_h)
         stream = ffmpeg.filter(stream, "scale", des_w,
@@ -180,7 +231,7 @@ frame_ffmpeg = tk.Frame(root, bd=1, padx=12, pady=3,
 frame_ffmpeg.grid(row=3, column=0, sticky="nesw")
 
 button_ffmpeg = tk.Button(
-    frame_ffmpeg, text='Render', command=ffmpeg_export, state="disabled", width=15, height=3)
+    frame_ffmpeg, text='Render', command=ffmpeg_export_meta, state="disabled", width=15, height=3)
 button_ffmpeg.grid(row=0, sticky="ew")
 progress_ffmpeg = tk.Message(frame_ffmpeg, text='Rendering: N/A', width=400)
 progress_ffmpeg.grid(row=1, sticky="ew")
